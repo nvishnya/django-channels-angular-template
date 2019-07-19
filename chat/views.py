4479c2aae9086433bcdb55df889dbe9e43df85b6
Nastya Liashchevska <nnvishnya@gmail.com>
2019-07-19 17:20:27 +0300

    Create MessageListView

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from chat.serializers import MessageSerializer
+from rest_framework import generics
+from chat.models import Message
 
-# Create your views here.
+
+class MessagesListView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        return Message.objects.filter(room_name=self.kwargs["room_name"])
