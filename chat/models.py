9263ccec351aeae54af84ff12c43f8016860cdaf
Nastya Liashchevska <nnvishnya@gmail.com>
2019-07-19 15:43:10 +0300

    Create the Message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    room_name = models.TextField(max_length=50)
+    text = models.TextField(max_length=300)
