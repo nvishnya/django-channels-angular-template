7c1e2fd2a033c528987f8577ca41b11860ba3493
Nastya Liashchevska <nnvishnya@gmail.com>
2019-08-02 17:40:52 +0300

    Add urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,9 @@
 from chat.views import MessagesListView
 from django.urls.conf import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('api/messages/<room_name>/', MessagesListView.as_view()),
+    path("", TemplateView.as_view(template_name="index.html")),
+    path("<room_name>/", TemplateView.as_view(template_name="index.html")),
+    path("api/messages/<room_name>/", MessagesListView.as_view()),
 ]
